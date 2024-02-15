e921cec62f20ba3c4f01125b8217fe8697c2aec0
obid1120 <yoqubovobidjon99@gmail.com>
2024-02-15 14:18:52 +0500

    added database file and modified some features

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -7,3 +7,8 @@ command_router = Router()
 @command_router.message(CommandStart())
 async def start_handler(message: Message):
     await message.answer("Assalomu alaykum!\n\nBotga xush kelibsiz")
+
+
+@command_router.message(Command('help'))
+async def help_handler(message: Message):
+    await message.answer("How can I help you?")
